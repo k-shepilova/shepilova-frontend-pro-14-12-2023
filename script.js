140d66c79458227371f3239ffb434b81badb3ecb
Kateryna Shepilova <152919831+k-shepilova@users.noreply.github.com>
2023-12-14 20:05:16 +0200

    fixed function

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function geRadiusOfCircle(promptText) {
-  return parseInt(prompt(promptText));
+function getRadiusOfCircle() {
+  const circleRadius = parseInt(prompt('Please enter the radius of circle'));
+  const areaOfCircle = Math.PI * Math.pow(circleRadius, 2);
+  console.log(`The area of circle is ${areaOfCircle}`);
 }
 
-const circleRadius = geRadiusOfCircle('Please enter the radius of circle');
-const areaOfCircle = Math.PI * Math.pow(circleRadius, 2);
-
-console.log(`The area of circle is ${areaOfCircle}`);
-
+getRadiusOfCircle();
